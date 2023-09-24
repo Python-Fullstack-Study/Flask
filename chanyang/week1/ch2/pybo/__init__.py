6aac96c111a7dfa4d97e9e82ff8bfc3d5e7b7b96
imchanyang <cksdid636@naver.com>
2023-09-24 18:12:36 +0900

    add ORM

--- chanyang/week1/ch2/pybo/__init__.py
+++ chanyang/week1/ch2/pybo/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
+import chanyang.week1.ch2.config as config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Blueprint
     from .views import main_view
     app.register_blueprint(main_view.bp)
 
--- chanyang/week1/ch2/pybo/views/main_view.py
+++ chanyang/week1/ch2/pybo/views/main_view.py
@@ -2,7 +2,10 @@ from flask import Blueprint
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-
 @bp.route('/')
+def index():
+    return 'Pybo index'
+
+@bp.route('/hello')
 def hello_pybo():
     return 'Hello, Pybo!'
